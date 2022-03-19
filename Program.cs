900235088828698041568b6df3782d5f5ba87d40
Anastassia Kuhartsik <nastjonka187@gmail.com>
2022-03-19 12:29:59 +0200

    add my project 3

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DrivingLicence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prilozenie prosit polzovatelja vvesti ego god rozdenija
+            //prilozenie vishisljaet vozrast polzovatelja (2022 - god rozdenija polzovatelja)
+            //esli polzovatel moloze 18 let, to v konsoli vivoditsa "too young to drive"
+            //esli polzovatel starwe 18 let,to v konsoli vivoditsa "drive carefully"
+            //a esli polzovatelju 18 let, to v konsoli vivoditsa "Congratulations,you my apply
+            //for your deiving licence now"
+
+            Console.WriteLine("Please enter your year of birth:");
+            int result = 2022 - Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+            if (result < 18)
+
+
+            {
+                Console.WriteLine("You are too young to drive.");
+
+            }
+             else if (result > 18)
+            {
+                Console.WriteLine("drive csrefully!");
+
+            }
+            else
+            {
+                Console.WriteLine("Congratulations,you my apply for your driving licence now");
+
+            }
+           
+        }
+        
+    }
+}
